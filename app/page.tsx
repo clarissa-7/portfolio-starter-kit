21aef7fa3435ea82bbe8f72db6192496861d695c
clarissa-7 <clarissabouassi7@gmail.com>
2025-05-10 14:42:46 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,15 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        The Horizon Residences Portal: Keeping Our Community Connected
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`As a Strata Committee, we believe in proactive and transparent management, 
+        finding efficiency in clear processes and open communication to address the
+        diverse needs of our residents. This extends to our commitment to financial
+        responsibility, where early and diligent planning ensures the long-term stability 
+        of our building, and our preference for resident feedback, which eases community 
+        engagement and strengthens our collaborative environment.`}
       </p>
       <div className="my-8">
         <BlogPosts />
