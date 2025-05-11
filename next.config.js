b34c0a133d6234bd62283bfdf13e17a2a8d919af
clarissa-7 <clarissabouassi7@gmail.com>
2025-05-11 23:28:07 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    runtime: 'nodejs', // Attempt to default API routes to Node.js
+    mdxRs: true // Enables MDX Rust compiler for faster builds, required for next-mdx-remote
   },
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'], // Supports MDX files for blog posts
+  // Add any other necessary options here
 };
 
 module.exports = nextConfig;
